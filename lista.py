2767619bac1363f859a3a7fa1bccac903aca352f
dayanaramendoza <dayanaramendozaarnez18@gmail.com>
2025-08-25 08:28:52 -0400

    Add files via upload

---
+++ lista.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+from tkinter import messagebox
+ventana =tk.Tk()
+ventana.title("Ejemplo Listbox")
+sintomasLabel=tk.Label(ventana, text="Síntomas")
+sintomasLabel.grid(row=0, column=0, padx=5, pady=5, sticky="w")
+#Crear Listbox
+lista= tk.Listbox(ventana, selectmode=tk.SINGLE)
+lista.insert(1, "Dolor de cabeza")
+lista.insert(2, "Fiebre")
+lista.insert(3, "Tos")
+lista.insert(4, "Fatiga")
+lista.insert(5, "Dificultad al respirar")
+lista.grid(row=0, column=1, pady=10, sticky="we")
+#Boton para mostrar seleccion
+def mostrar():
+    seleccionado=lista.get(lista.curselection())
+    tk.messagebox.showinfo("Seleccion", f"Has elegido: {seleccionado}")
+boton=tk.Button(ventana, text="Mostrar seleccion", command=mostrar)
+boton.grid(row=1, column=0, padx=10, pady=10)
+
+ventana.mainloop()
