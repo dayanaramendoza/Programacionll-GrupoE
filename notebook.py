f35dfd850c79c402829a44cc6ab2b7f070355ca4
dayanaramendoza <dayanaramendozaarnez18@gmail.com>
2025-09-01 08:48:28 -0400

    Add files via upload

--- notebook.py
+++ notebook.py
@@ -3,7 +3,7 @@ from tkinter import ttk, messagebox
 #CREAR VENTANA PRINCIPAL
 ventana_principal=tk.Tk()
 ventana_principal.title("Libro de Pacientes y Doctores")
-ventana_principal.geometry("600x800")
+ventana_principal.geometry("1000x800")
 #CREAR CONTENEDOR NOTEBOOK (PESTAÑAS)
 pestañas=ttk.Notebook(ventana_principal)
 #CREAR FRAMES (UNO POR PESTAÑA)
@@ -52,10 +52,96 @@ tipo_seguro.set("Publico") #VALOR POR DEFECTO
 comboTipoSeguro=ttk.Combobox(frame_pacientes, values=["Publico", "Privado", "Ninguno"], textvariable=tipo_seguro)
 comboTipoSeguro.grid(row=5, column=1, sticky="w", pady=5, padx=5)
 #CENTRO MEDICO
-labelCentroMedico=tk.Label(frame_pacientes, text="Cenro de salud: ")
+labelCentroMedico=tk.Label(frame_pacientes, text="Centro de salud: ")
 labelCentroMedico.grid(row=6, column=0, sticky="w", pady=5, padx=5)
 centro_medico=tk.StringVar()
 centro_medico.set("Hospital Central") #Valor por defecto
 comboCentroMedico=ttk.Combobox(frame_pacientes, values=["Hospital Central", "Clinica Norte", "Centro Sur"], textvariable=centro_medico)
 comboCentroMedico.grid(row=6, column=1, sticky="w", padx=5, pady=5)
+#Frame para los botones
+btn_frame=tk.Frame(frame_pacientes)
+btn_frame.grid(row=8, column=0, columnspan=2, pady=5, sticky="w")
+#BOTON REGISTRAR
+btn_registrar=tk.Button(btn_frame, text="Registrar", command="")
+btn_registrar.grid(row=0, column=0, padx=5)
+#BOTON ELIMINAR
+btn_eliminar=tk.Button(btn_frame, text="Eliminar", command="")
+btn_eliminar.grid(row=0, column=1, padx=5)
+#CREAR TREEVIEW PARA MOSTRAR PACIENTES
+treeview=ttk.Treeview(frame_pacientes, columns=("Nombre", "FechaN", "Edad", "Genero", "GrupoS", "TipoS", "CentroM"), show="headings")
+#DEFINIR ENCABEZADOS
+treeview.heading("Nombre", text="Nombre Completo")
+treeview.heading("FechaN", text="Fecha Nacimiento")
+treeview.heading("Edad", text="Edad")
+treeview.heading("Genero", text="Genero")
+treeview.heading("GrupoS", text="Grupo Sanguineo")
+treeview.heading("TipoS", text="Tipo Seguro")
+treeview.heading("CentroM", text="Centro Medico")
+#DEFINIR ANCHOS DE COLUMNAS
+treeview.column("Nombre", width=120)
+treeview.column("FechaN", width=120)
+treeview.column("Edad", width=50, anchor="center")
+treeview.column("Genero", width=60, anchor="center")
+treeview.column("GrupoS", width=100, anchor="center")
+treeview.column("TipoS", width=100, anchor="center")
+treeview.column("CentroM", width=120)
+#UBICAR EL TREEVIEW EN LA CUADRICULA
+treeview.grid(row=7, column=0, columnspan=2, sticky="nsew", padx=5, pady=10)
+#SCROLLBAR VERTICAL
+scroll_y=ttk.Scrollbar(frame_pacientes, orient="vertical", command=treeview.yview)
+treeview.configure(yscrollcommand=scroll_y.set)
+scroll_y.grid(row=7, column=2, sticky="ns")
+#FRAME DOCTORES
+#NOMBRE
+labelNombreD=tk.Label(frame_doctores, text="Nombre: ")
+labelNombreD.grid(row=0, column=0, sticky="w", padx=5, pady=5)
+nombreD=tk.Entry(frame_doctores)
+nombreD.grid(row=0, column=1, sticky="w", padx=5, pady=5)
+#ESPECIALIDAD
+labelEspecialidad=tk.Label(frame_doctores, text="Especialidad: ")
+labelEspecialidad.grid(row=2, column=0, sticky="w", pady=5, padx=5)
+Especialidad=tk.StringVar()
+Especialidad.set("Cardiología") #VALOR POR DEFECTO
+comboEspecialidad=ttk.Combobox(frame_doctores, values=["Pediatría", "Neurología", "Traumatología", "Oftalmología"], textvariable=Especialidad)
+comboEspecialidad.grid(row=2, column=1, sticky="w", pady=5, padx=5)
+#EDAD
+labelEdadD=tk.Label(frame_doctores, text="Edad: ")
+labelEdadD.grid(row=3, column=0, sticky="w", pady=5, padx=5)
+valores=list(range(25, 81))
+edadD=tk.Spinbox(frame_doctores, values=(valores))
+edadD.grid(row=3, column=1, padx=10, pady=10, sticky="w")
+#TELEFONO
+labelTelefono=tk.Label(frame_doctores, text="Telefono: ")
+labelTelefono.grid(row=4, column=0, sticky="w", padx=5, pady=5)
+Telefono=tk.Entry(frame_doctores)
+Telefono.grid(row=4, column=1, sticky="w", padx=5, pady=5)
+#Frame para los botones
+boton_frame=tk.Frame(frame_doctores)
+boton_frame.grid(row=5, column=0, columnspan=2, pady=5, sticky="w")
+#BOTON REGISTRAR
+boton_registrar=tk.Button(boton_frame, text="Registrar", command="")
+boton_registrar.grid(row=5, column=0, padx=5)
+boton_registrar.configure(background="green")
+#BOTON ELIMINAR
+boton_eliminar=tk.Button(boton_frame, text="Eliminar", command="")
+boton_eliminar.grid(row=5, column=1, padx=5)
+boton_eliminar.configure(background="blue")
+#CREAR TREEVIEW PARA MOSTRAR DOCTORES
+TreevieW=ttk.Treeview(frame_doctores, columns=("Nombre", "Especialidad", "Edad", "Telefono"), show="headings")
+#DEFINIR ENCABEZADOS
+TreevieW.heading("Nombre", text="Nombre Completo")
+TreevieW.heading("Especialidad", text="Especialidad")
+TreevieW.heading("Edad", text="Edad")
+TreevieW.heading("Telefono", text="Telefono")
+#DEFINIR ANCHOS DE COLUMNAS
+TreevieW.column("Nombre", width=120)
+TreevieW.column("Especialidad", width=120)
+TreevieW.column("Edad", width=50, anchor="center")
+TreevieW.column("Telefono", width=60, anchor="center")
+#UBICAR EL TREEVIEW EN LA CUADRICULA
+TreevieW.grid(row=6, column=0, columnspan=2, sticky="nsew", padx=5, pady=10)
+#SCROLLBAR VERTICAL
+Scroll_y=ttk.Scrollbar(frame_doctores, orient="vertical", command=TreevieW.yview)
+TreevieW.configure(yscrollcommand=Scroll_y.set)
+Scroll_y.grid(row=6, column=2, sticky="ns")
 ventana_principal.mainloop()
